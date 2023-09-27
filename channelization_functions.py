367a9a5ac489cbb822d43f749e45fd55f7f52d1b
Olivia Pereira <130678573+oliviapereira2002@users.noreply.github.com>
2023-09-27 11:08:00 -0400

    updated get_fine_freqs

--- channelization_functions.py
+++ channelization_functions.py
@@ -167,9 +167,12 @@ def read_catalogue(file):
 
     return V, S, z, ra, dec
 
-def get_fine_freqs(observing_freqs, fmin, fmax):
-    dc = np.abs(observing_freqs[0] - observing_freqs[1]) # getting a negative dc 
+def get_fine_freqs(observing_freqs):
+    fmax = np.max(observing_freqs)
+    fmin = np.min(observing_freqs)
+    dc = observing_freqs[1] - observing_freqs[0]  # getting a negative dc 
     return np.arange(fmax, fmin, dc / 3) # making the frequency resolution = 1/3 dc 
+
 
 def get_resampled_profiles(V, S, z, fine_freqs):
     '''Takes opened galaxy catalogue and returns finely re-sampled profiles in frequency space.
